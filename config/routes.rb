41737ccee4627f267b489b05218aa36ee194b85e
ayumi0511 <ayumi.tokoeda@gmail.com>
2023-09-11 16:00:03 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "rooms#index"
-  resources :rooms, only: [:new, :create] do
+  resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
 end
